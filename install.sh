f54b91f5cbf99d9254432040a31a2ce031802d5a
Andreas Braun <git@alcaeus.org>
2016-07-04 08:08:10 +0200

    Add some output to install script

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Install brewfiles"
 brew bundle
 brew bundle --file=Caskfile
 
+echo "Bootstrap settings"
 ./bootstrap.sh
