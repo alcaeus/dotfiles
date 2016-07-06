d9148b9fdcc45d026e2bf0d5892aa66c98eede68
Andreas Braun <git@alcaeus.org>
2016-07-06 13:03:08 +0200

    Use standard function keys

--- macos-settings/02-io.sh
+++ macos-settings/02-io.sh
@@ -19,6 +19,9 @@ defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 # Follow the keyboard focus while zoomed in
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
+# Enable standard function keys
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
