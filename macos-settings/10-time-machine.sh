e00d9cd2fe9d47383530f78e24f7dbcc237411f9
Andreas Braun <git@alcaeus.org>
2019-01-01 11:08:46 +0100

    Drop macos settings

--- macos-settings/10-time-machine.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-###############################################################################
-# Time Machine                                                                #
-###############################################################################
-
-# Prevent Time Machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
